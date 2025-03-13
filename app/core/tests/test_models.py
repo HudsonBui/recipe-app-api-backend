63ac8c8c6f58c3cc74964def51ded41510bd4c8b
Hudson Bui <hudsonbui369vn@gmail.com>
2025-03-14 00:19:21 +0700

    create superuser and test for creating superuser

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -40,3 +40,14 @@ class ModelTest(TestCase):
         """
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'value123')
+
+    def test_create_superuser(self):
+        """Test creating a superuser."""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123'
+        )
+
+        # is_superuser is a field that provides by PermissionMixin
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
